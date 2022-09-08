22ae062a070f30dbb858a16c2131495a36a320bd
RominaGonella <rominagonfur@gmail.com>
2022-09-08 23:37:15 +0000

    ajustes a la versión final

--- src/pages/8_🤵_Entidades.py
+++ src/pages/8_🤵_Entidades.py
@@ -34,9 +34,10 @@ st.plotly_chart(fig, use_container_width=True, showlegend= False)
 
 st.markdown('')
 st.markdown('* ##### Las principales entidades identificadas son:')
-st.markdown('###### **GPE:** Compañías, instituciones, países, etc.')
-st.markdown('###### **NORP:** Nacionalidades, grupos religiosos y políticos.')
-st.markdown('###### **PERSONA:** Personas (incluidas ficticias).')
+st.markdown('###### **GPE:** Países, ciudades o estados')
+st.markdown('###### **NORP:** Nacionalidades, grupos religiosos y políticos')
+st.markdown('###### **PERSON:** Personas (incluidas ficticias)')
+st.markdown('###### **ORG:** Compañías, agencias, instituciones')
 st.markdown('')
 st.markdown('')
 
@@ -45,7 +46,7 @@ st.markdown(tit, unsafe_allow_html=True)
 
 # ENTIDAD GPE
 
-tit_2 = '<p style="font-family:Arial; color:Black; font-size: 40px;">Compañías , instituciones o países </p>'
+tit_2 = '<p style="font-family:Arial; color:Black; font-size: 40px;">Países, ciudades o estados </p>'
 st.markdown(tit_2, unsafe_allow_html=True)
 
 # load object
@@ -89,7 +90,7 @@ fig.update_layout(coloraxis=dict(colorscale='Bluered_r'), showlegend=True)
 
 st.plotly_chart(fig, use_container_width=True, showlegend= False)
 
-# ENTIDAD PERSONA
+# ENTIDAD PERSON
 
 tit_1 = '<p style="font-family:Arial; color:Black; font-size: 40px;">Personas </p>'
 st.markdown(tit_1, unsafe_allow_html=True)
@@ -104,6 +105,29 @@ with open('data/interim/count_person_z.pkl' , 'rb') as f:
 
 x_p,y_p=map(list,zip(*count_person_p))
 x_z,y_z=map(list,zip(*count_person_z))
+
+fig = make_subplots(rows=1, cols=2, shared_xaxes=True, shared_yaxes=True)
+fig.add_trace(go.Bar(x=x_p, y=y_p, name='Putin'),1,1)
+fig.add_trace(go.Bar(x=x_z, y=y_z, name='Zelensky'),1, 2)
+fig.update_layout(coloraxis=dict(colorscale='Bluered_r'), showlegend=True)
+
+st.plotly_chart(fig, use_container_width=True, showlegend= False)
+
+# ENTIDAD ORG
+
+tit_1 = '<p style="font-family:Arial; color:Black; font-size: 40px;">Compañías, agencias, instituciones </p>'
+st.markdown(tit_1, unsafe_allow_html=True)
+
+# load object
+with open('data/interim/count_org_p.pkl' , 'rb') as f:
+    count_org_p = pickle.load(f)
+
+# load object
+with open('data/interim/count_org_z.pkl' , 'rb') as f:
+    count_org_z = pickle.load(f)
+
+x_p,y_p=map(list,zip(*count_org_p))
+x_z,y_z=map(list,zip(*count_org_z))
 
 fig = make_subplots(rows=1, cols=2, shared_xaxes=True, shared_yaxes=True)
 fig.add_trace(go.Bar(x=x_p, y=y_p, name='Putin'),1,1)
